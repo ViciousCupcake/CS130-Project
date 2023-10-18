427fd36af50db30e72f4215159e5982a64354049
ViciousCupcake <22625405+ViciousCupcake@users.noreply.github.com>
2023-10-18 16:58:51 -0700

    Add framework to allow people to build off of

--- cs130_efi/settings.py
+++ cs130_efi/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'cs130_efi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
