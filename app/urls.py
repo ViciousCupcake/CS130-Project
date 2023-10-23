6743e867e19e6976b6629aa41f6464d024e4c5c6
Jonathan Xu <22625405+ViciousCupcake@users.noreply.github.com>
2023-10-22 17:06:41 -0700

    Add mapping model (#13)
    * Bootstrap docker,django,postgres,fuseki
    
    * add new line to EOF
    
    * clean up docker compose
    
    * Add framework to allow people to build off of
    
    * add mapping model

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("list/", views.list, name='list'),
+    path("import/", views.import_mapping, name="import"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
-
+from .models import Mapping
+from .forms import MappingForm
 
 def index(request):
     return render(request, "app/index.html", {'data': 'Hello, world!'})
+
+def list(request):
+    mappings = Mapping.objects.all()
+    return render(request, "app/list.html", {"mappings": mappings})
+
+def import_mapping(request): 
+    form = MappingForm(request.POST or None, request.FILES or None)
+     
+    if form.is_valid():
+        form.save()
+        return render(request, "app/save_success.html", {'mapping_title': form.cleaned_data['title']})
+ 
+    return render(request, "app/import.html", {'form': form})
