75898a5fb23977d0181c18c33e966a1418d92239
Jonathan Xu <22625405+ViciousCupcake@users.noreply.github.com>
2023-10-27 16:15:52 -0700

    Add authentication (#41)
    * allow users to login and logout
    
    * add comment describing what urls are implemented
    
    * add auth unit test

--- app/tests.py
+++ app/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from app.models import Mapping
 
 class EFITestCase(TestCase):
@@ -19,3 +21,36 @@ class EFITestCase(TestCase):
         self.assertEqual(saved_mapping.description, "Test description")
         self.assertEqual(saved_mapping.fuseki_query, "Test query")
         self.assertEqual(saved_mapping.excel_format, {"test": "format"})
+
+    def test_auth(self):
+        """Test authentication."""
+
+        user = User.objects.create_user(username='test', password='test')
+        self.client.login(username='test', password='test')
+
+        signed_in_user = authenticate(username='test', password='test')
+        self.assertEqual(user, signed_in_user)
+
+    def test_auth_fail(self):
+        """Test authentication failure."""
+
+        user = User.objects.create_user(username='test', password='test')
+        self.client.login(username='test', password='wrong_password')
+
+        signed_in_user = authenticate(username='test', password='wrong_password')
+        self.assertNotEqual(user, signed_in_user)
+    
+    def test_auth_reachable(self):
+        """Test authentication page reachable."""
+
+        response = self.client.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout(self):
+        """Test logout."""
+
+        User.objects.create_user(username='test', password='test')
+        self.client.login(username='test', password='test')
+
+        response = self.client.get('/accounts/logout/')
+        self.assertEqual(response.status_code, 302)
--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.urls import include
 
 from . import views
 
@@ -6,4 +7,16 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("list/", views.list, name='list'),
     path("import/", views.import_mapping, name="import"),
+
+    # This defines:
+        # accounts/login/ [name='login']
+        # accounts/logout/ [name='logout']
+    # These paths are yet unimplemented:
+        # accounts/password_change/ [name='password_change']
+        # accounts/password_change/done/ [name='password_change_done']
+        # accounts/password_reset/ [name='password_reset']
+        # accounts/password_reset/done/ [name='password_reset_done']
+        # accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+        # accounts/reset/done/ [name='password_reset_complete']
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
