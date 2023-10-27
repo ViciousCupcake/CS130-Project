53fd76ea5e8a57afdeaf5c36770f3bff93598c1c
ViciousCupcake <22625405+ViciousCupcake@users.noreply.github.com>
2023-10-26 23:23:48 -0700

    unit tests

--- app/tests.py
+++ app/tests.py
@@ -1,7 +1,20 @@
 from django.test import TestCase
+from .forms import MappingForm
 
 class ExampleTestCase(TestCase):
     def test_example(self):
         """Example test case."""
 
         self.assertEqual(2, 1+1)
+
+    def test_form_validation_fail(self):
+        """Test Mapping ModelForm validation."""
+
+        form = MappingForm(data={'title': 'test', 'description': 'test', 'fuseki_query': 'test', 'excel_format': 'test'})
+        self.assertFalse(form.is_valid())
+
+    def test_form_validation_success(self):
+        """Test Mapping ModelForm validation."""
+
+        form = MappingForm(data={'title': 'test', 'description': 'test', 'fuseki_query': 'test', 'excel_format': '{"test": "data"}'})
+        self.assertTrue(form.is_valid())
